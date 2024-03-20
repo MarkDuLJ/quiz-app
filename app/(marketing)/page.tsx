e1ba449ff003374306924d588a45e6c9903be3da
Mark Du <dulongjiebackup@gmail.com>
2024-03-20 12:16:51 -0400

    clerk status

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,11 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full mx-auto flex flex-col max-w-[988px] lg:flex-row items-center justify-center p-4 gap-2">
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
         <Image src="./next.svg" alt="hero" fill/>
+      </div>
+      <div>
+        <ClerkLoading>
+          <Loader className="w-5 h-5"/>
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedOut>
+            <SignUpButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+              <Button size="lg" variant="secondary" className="w-full">
+                Get Start
+              </Button>
+            </SignUpButton>
+            <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+              <Button size="lg" variant="primaryOutline" className="w-full">
+                Already have an account
+              </Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <Button size="lg" variant="secondary" className="w-full" asChild>
+              <Link href="/learn">
+              Continue your study
+              </Link>
+            </Button>
+          </SignedIn>
+        </ClerkLoaded>
       </div>
     </div>
   );
