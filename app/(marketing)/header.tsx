aa6e2885f27d5187ff90288ea5e82457f5c418fa
Ldeng928 <lideng928@gmail.com>
2024-03-23 14:13:32 -0400

    Adjusted layouts for sidebar, main, and header

--- app/(marketing)/header.tsx
+++ app/(marketing)/header.tsx
@@ -5,23 +5,25 @@ import Image from "next/image"
 
 export const Header = () => {
     return (
-        <header className="h-20 w-full border-b-2 border-slate-200 px-4">
-            <div className="flex items-center justify-between mx-auto h-full lg:max-w-screen-xl bg-green-200">
+        <header className="h-20 w-full border-b-2 border-slate-200 px-4 bg-green-200">
+            <div className="flex items-center justify-between mx-auto h-full lg:max-w-screen-xl">
             <div className="flex items-center gap-x-3">
                 <Image src="/next.svg" alt="img" width={80} height={80}/>
                 <h1 className="text-2xl font-extrabold tracking-wide text-green-600">Quiz</h1>
             </div>
-            <ClerkLoading>
-                <Loader className="h-5 w-5 text-muted-foreground"/>
-            </ClerkLoading>
-            <ClerkLoaded>
-                <SignedIn>
-                    <UserButton afterSignOutUrl="/"/>
-                </SignedIn>
-                <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
-                    <Button variant="ghost">Login</Button>
-                </SignInButton>
-            </ClerkLoaded>
+            <div className="ml-auto flex items-center">
+                <ClerkLoading>
+                    <Loader className="h-5 w-5 text-muted-foreground"/>
+                </ClerkLoading>            
+                <ClerkLoaded>
+                    <SignedIn>
+                        <UserButton afterSignOutUrl="/"/>
+                    </SignedIn>
+                    <SignInButton mode="modal" afterSignInUrl="/learn" afterSignUpUrl="/learn">
+                        <Button variant="ghost">Login</Button>
+                    </SignInButton>
+                </ClerkLoaded>
+            </div>
             </div>
         </header>
     )
--- components/side-bar.tsx
+++ components/side-bar.tsx
@@ -7,26 +7,29 @@ import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
 
 export const Sidebar =()=>{
     return (
-        <div className="flex flex-col h-full lg:w-[256px] left-0 top-0 px-4 border-r-2">
-            <Link href="/learn">
-                <div className="flex items-center pt-10 pl-4 pb-8 gap-x-2">
-                    <AirplayIcon  height={30} width={30} className="pt-1"/>
-                    <h1 className="text-2xl font-extrabold text-green-500 tracking-wide">quiz</h1>
-                </div>
-            </Link>
-            <div className="flex flex-col gap-y-3 flex-1">
-                <SidebarButton href="/learn" label="Learn" iconSrc="/leaderboard.svg"/>
-                <SidebarButton href="/dashboard" label="dashboard" iconSrc="/leaderboard.svg"/>
-                <SidebarButton href="/shop" label="shop" iconSrc="/leaderboard.svg"/>
+        <>
+
+            <div className="flex flex-col gap-y-3 flex-wrap items-start border-r-2">
+                {/* <div>
+                        <ClerkLoading>
+                            <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
+                        </ClerkLoading>
+                        <ClerkLoaded>
+                            <UserButton afterSignOutUrl="/" />
+                        </ClerkLoaded>
+                    </div> */}
+                
+                    <Link href="/learn">
+                        <div className="flex items-center pt-6 pl-4 pb-8 gap-x-2">
+                            <AirplayIcon height={30} width={30} className="pt-1" />
+                            <h1 className="text-2xl font-extrabold text-green-500 tracking-wide">quiz</h1>
+                        </div>
+                    </Link>
+                    <SidebarButton href="/learn" label="Learn" iconSrc="/leaderboard.svg" />
+                    <SidebarButton href="/dashboard" label="dashboard" iconSrc="/leaderboard.svg" />
+                    <SidebarButton href="/shop" label="shop" iconSrc="/leaderboard.svg" />
+                
             </div>
-            <div className="p-4">
-                <ClerkLoading>
-                    <Loader className="h-5 w-5 text-muted-foreground animate-spin"/>
-                </ClerkLoading>
-                <ClerkLoaded>
-                    <UserButton afterSignOutUrl="/" />
-                </ClerkLoaded>
-            </div>
-        </div>
+        </>
     )
 }
